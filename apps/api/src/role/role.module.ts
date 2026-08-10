4d23a566922d11c203c02e08cf18ae42977a3c1f
irmaliadk <irmaliadk@student.ub.ac.id>
2026-08-10 14:21:18 +0700

    feat(api): modul Role dan Permission dengan CRUD serta assign izin

---
+++ apps/api/src/role/role.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RoleController } from './role.controller.js';
+import { RoleService } from './role.service.js';
+
+@Module({
+  controllers: [RoleController],
+  providers: [RoleService],
+})
+export class RoleModule {}
--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -1,2 +1,3 @@
 export * from './error.js';
 export * from './auth/login.js';
+export * from './role/role.js';
